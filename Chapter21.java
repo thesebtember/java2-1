7d17ad9f0340c31ff438c91671799aa7ed485d55
thesebtember <chekroun123@naver.com>
2024-03-22 12:45:42 +0900

    3월 22일

---
+++ Chapter21.java
@@ -0,0 +1,6 @@
+// 예제2-1  
+public class Chapter21 {
+    public static void main(String[] args) {
+        public 
+    }
+}
--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("println1");
+        System.out.println("println2");
+        System.out.println("print1");
+        System.out.println("print2");
     }
 }
